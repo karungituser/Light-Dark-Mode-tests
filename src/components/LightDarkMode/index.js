6eb41cde30da5184b8737c9c0eb7eb3d2c6cb6bb
karungituser <karunkumarkkd@gmail.com>
2022-06-16 11:56:41 +0000

    Light Dark Mode tests Solution

---
+++ src/components/LightDarkMode/index.js
@@ -0,0 +1,31 @@
+// Write your code here
+
+import {Component} from 'react'
+import './index.css'
+
+class LightDarkMode extends Component {
+  state = {isDarkMode: true}
+
+  onClickButton = () => {
+    this.setState(prevState => ({isDarkMode: !prevState.isDarkMode}))
+  }
+
+  render() {
+    const {isDarkMode} = this.state
+    const darkModeChanged = isDarkMode ? 'dark-mode' : 'light-mode'
+    const buttonText = isDarkMode ? 'Light Mode' : 'Dark Mode'
+
+    return (
+      <div className="bg-container">
+        <div className={`${darkModeChanged} lightDark-container`}>
+          <h1 className="heading">Click To Change Mode</h1>
+          <button type="button" className="button" onClick={this.onClickButton}>
+            {buttonText}
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default LightDarkMode
